a26ec0bcb711c11c12dd478dc288195276bd8fe3
apeksha-mahale <mahaleapeksha@gmail.com>
2018-03-16 19:49:30 +0530

    Second  commit

--- app/src/main/java/com/example/clientapp/MainActivity.java
+++ app/src/main/java/com/example/clientapp/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database=FirebaseDatabase.getInstance();
-        DatabaseReference myRef= database.getReference("Loction");
+        final FirebaseDatabase database=FirebaseDatabase.getInstance();
+        DatabaseReference myRef= database.getReference("Location");
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
