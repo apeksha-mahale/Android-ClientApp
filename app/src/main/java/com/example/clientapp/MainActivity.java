5fa5d34291ec6742e69e5e0535f11a12a06fa396
apeksha-mahale <mahaleapeksha@gmail.com>
2018-05-31 09:37:03 +0530

    6th commit

--- app/src/main/java/com/example/clientapp/MainActivity.java
+++ app/src/main/java/com/example/clientapp/MainActivity.java
@@ -52,9 +52,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 value=dataSnapshot.getValue(String.class);
-
                 Log.i("VaLue", value);
-                /*textView.setText(value+"Hi");*/
                 String [] separated = value.split(",");
 
             }
@@ -76,129 +74,152 @@ public class MainActivity extends AppCompatActivity {
 
     public void findClosestLocation() {
         final int[] closestAmb = {0};
+        final int flag=0;
         final String[] locArray = new String[3];
-        for(int x=0;x<=2;x++){
+        for(int x=0;x<=2;x++) {
+            final int m = x;
             DatabaseReference myR = FirebaseDatabase.getInstance().getReference("AmbulanceList");
-            DatabaseReference DLoc = myR.child(x+"");
-            DatabaseReference DDLoc= DLoc.child("Location");
-
-            final int m=x;
-            DDLoc.addValueEventListener(new ValueEventListener() {
+            final DatabaseReference DLoc5 = myR.child(x + "");
+            DatabaseReference DDStatus = DLoc5.child("State");
+            DDStatus.addValueEventListener(new ValueEventListener() {
                 @Override
+
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    ambValue[0] = dataSnapshot.getValue(String.class);
-                    Log.i("Coordinates", ambValue[0]);
-                    locArray[m]=ambValue[0];
-                    Log.i("Coordinates of m", Integer.toString(m)+" : " +ambValue[0]+" , " +locArray[m]);
-                    Double[] dis = new Double[3];
-                    int j = 0;
-                    Double minDist=Double.MAX_VALUE;
-                    if(m==2) {
-
-                        for(int y=0;y<=2;y++)
-                            Log.i("Coordinates of y", Integer.toString(y) + " , " + locArray[y]);
-                        int minAmb = -1;
-                        for (String eachLoc : locArray) {
-                            String[] latlong = new String[1];
-                            if (eachLoc != null) {
-                                latlong = eachLoc.split(",");
-                                double latitude = Double.parseDouble(latlong[0]);
-                                double longitude = Double.parseDouble(latlong[1]);
-                                LatLng latLangLocation = new LatLng(latitude, longitude);
-                                Double distance = CalculateDistance(latLangLocation);
-                                Log.i("Distance"+Integer.toString(j)+" : ", Double.toString(distance));
-                                dis[j] = distance;
-                                Log.i("Coordinates of distance"+Integer.toString(j)+" : ", "Distance is " + Double.toString(dis[j]));
-                                j++;
-                            }
-
-                        }
-                        double minDistance = Collections.min(Arrays.asList(dis));
-                        if(minDistance==dis[0])minAmb=0;
-                        else if(minDistance==dis[1])minAmb=1;
-                        else minAmb=2;
-                        Toast.makeText(getApplicationContext(), Integer.toString(minAmb),
-                                Toast.LENGTH_LONG).show();
-                        DatabaseReference myRef1 = FirebaseDatabase.getInstance().getReference("AmbulanceList");
-                        DatabaseReference zero = myRef1.child("PickupLoc");
-                        zero.setValue(locArray[minAmb]);
-                        av = minAmb;
-                        Log.i("Min Ambulance", Integer.toString(minAmb));
-                        TextView textView=(TextView) findViewById(R.id.LocText);
-                        /*textView.setText("Distances are : "+ Double.toString(dis[0])+ " "+ Double.toString(dis[1])+ " "+
-                                Double.toString(dis[2])+ " "+ "Minimum Ambulance is " + Integer.toString(minAmb));*/
-                        minAmbText.setText("Closest Ambulance is " + minAmb+"");
-                        Log.i("Log 1", "Before db");
-                        DatabaseReference myR = FirebaseDatabase.getInstance().getReference("AmbulanceList");
-                        DatabaseReference DLoc = myR.child(minAmb+"");
-                        DatabaseReference DDLoc= DLoc.child("PhoneNum");
-                        Log.i("Log 2", "After db "+minAmb+"");
+                    Log.i("State Value ",dataSnapshot.getValue(String.class) );
+                    final String statusVal = dataSnapshot.getValue(String.class);
+                    if (statusVal.equals("0")||m==2) {
+                        DatabaseReference DDLoc = DLoc5.child("Location");
+                        Log.i("State Value inside",dataSnapshot.getValue(String.class) );
                         DDLoc.addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(DataSnapshot dataSnapshot) {
-                                final String phoneNum = dataSnapshot.getValue(String.class);
+                                if (statusVal.equals("0")) {
+                                    ambValue[0] = dataSnapshot.getValue(String.class);
+                                    Log.i("Coordinates", ambValue[0]);
+                                    locArray[m] = ambValue[0];
+                                    Log.i("Coordinates of m", Integer.toString(m) + " : " + ambValue[0] + " , " + locArray[m]);
+                                }
+                                    Double[] dis = new Double[3];
+                                    int j = -1;
+                                    Double minDist = Double.MAX_VALUE;
 
-                                try {
+                                if (m == 2) {
 
-                                    Log.i("Log 3", "Before Toast");
-                                    DatabaseReference myR = FirebaseDatabase.getInstance().getReference("AmbulanceList");
-                                    DatabaseReference zero = myR.child("PickupLoc");
+                                    for (int y = 0; y <= 2; y++)
+                                        Log.i("Coordinates of y", Integer.toString(y) + " , " + locArray[y]);
+                                    for(int u=0;u<=2;u++)dis[u]= Double.MAX_VALUE;
+                                    int minAmb = -1;
+                                    for (String eachLoc : locArray) {
+                                        String[] latlong = new String[1];
+                                        j++;
+                                        if (eachLoc != null) {
+                                            latlong = eachLoc.split(",");
+                                            double latitude = Double.parseDouble(latlong[0]);
+                                            double longitude = Double.parseDouble(latlong[1]);
+                                            LatLng latLangLocation = new LatLng(latitude, longitude);
+                                            Double distance = CalculateDistance(latLangLocation);
+                                            Log.i("Distance" + Integer.toString(j) + " : ", Double.toString(distance));
+                                            dis[j] = distance;
+                                            Log.i("Coordinates of distance" + Integer.toString(j) + " : ", "Distance is " + Double.toString(dis[j]));
 
-                                    zero.addValueEventListener(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(DataSnapshot dataSnapshot) {
-                                            String value = null;
-                                            value = dataSnapshot.getValue(String.class);
-                                            SmsManager smsManager = SmsManager.getDefault();
-                                            String[] ltlng = value.split(",");
-                                           /* Uri.Builder builder = new Uri.Builder();
-                                            builder.scheme("https")
-                                                    .authority("www.google.com")
-                                                    .appendPath("maps")
-                                                    .appendPath("search")
-                                                    .appendQueryParameter("api", "1")
-                                                    .appendQueryParameter("map_action", "map")
-                                                    .appendQueryParameter("center", "1.732"+","+"7.732");*/
-                                            Uri builder = Uri.parse("http://maps.google.com/maps?daddr=" + ltlng[0] + "," + ltlng[1]);
-                                            String myUrl = builder.toString();
-                                            smsManager.sendTextMessage(phoneNum, null, "Go To Location "+ myUrl , null, null);
-                                            Log.i("Inside loop: ", value);
-                                            Toast.makeText(getApplicationContext(), "SMS Sent!",
-                                                    Toast.LENGTH_LONG).show();
-                                            Log.i("Log 4", "After toast");
                                         }
 
+                                    }
+                                    for(int u=0;u<=2;u++)Log.i("Coordinates of distance outside" + Integer.toString(j) + " : ", "Distance is " + Double.toString(dis[j]));
+                                    double minDistance = Collections.min(Arrays.asList(dis));
+                                    if (minDistance == dis[0]) minAmb = 0;
+                                    else if (minDistance == dis[1]) minAmb = 1;
+                                    else minAmb=2;
+                                    Toast.makeText(getApplicationContext(), Integer.toString(minAmb),
+                                            Toast.LENGTH_LONG).show();
+                                    DatabaseReference myRef1 = FirebaseDatabase.getInstance().getReference("AmbulanceList");
+                                    DatabaseReference zero = myRef1.child("PickupLoc");
+                                    zero.setValue(locArray[minAmb]);
+                                    DatabaseReference myR2 = FirebaseDatabase.getInstance().getReference("AmbulanceList");
+                                    DatabaseReference DLoc6 = myR2.child(minAmb + "");
+                                    DatabaseReference DDMinAmbStatus = DLoc6.child("State");
+                                    DDMinAmbStatus.setValue("1");
+                                    av = minAmb;
+                                    Log.i("Min Ambulance", Integer.toString(minAmb));
+                                    TextView textView = (TextView) findViewById(R.id.LocText);
+                                /*textView.setText("Distances are : "+ Double.toString(dis[0])+ " "+ Double.toString(dis[1])+ " "+
+                                        Double.toString(dis[2])+ " "+ "Minimum Ambulance is " + Integer.toString(minAmb));*/
+                                    minAmbText.setText("Closest Ambulance is " + minAmb + "");
+                                    Log.i("Log 1", "Before db");
+                                    DatabaseReference myR = FirebaseDatabase.getInstance().getReference("AmbulanceList");
+                                    DatabaseReference DLoc = myR.child(minAmb + "");
+                                    DatabaseReference DDLoc = DLoc.child("PhoneNum");
+                                    Log.i("Log 2", "After db " + minAmb + "");
+                                    DDLoc.addValueEventListener(new ValueEventListener() {
                                         @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            final String phoneNum = dataSnapshot.getValue(String.class);
+
+                                            try {
+
+                                                Log.i("Log 3", "Before Toast");
+                                                DatabaseReference myR = FirebaseDatabase.getInstance().getReference("AmbulanceList");
+                                                DatabaseReference zero = myR.child("PickupLoc");
+
+                                                zero.addValueEventListener(new ValueEventListener() {
+                                                    @Override
+                                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                                        String value = null;
+                                                        value = dataSnapshot.getValue(String.class);
+                                                        SmsManager smsManager = SmsManager.getDefault();
+                                                        String[] ltlng = value.split(",");
+                                                        Uri builder = Uri.parse("http://maps.google.com/maps?daddr=" + ltlng[0] + "," + ltlng[1]);
+                                                        String myUrl = builder.toString();
+                                                        smsManager.sendTextMessage(phoneNum, null, "Go To Location " + myUrl, null, null);
+                                                        Log.i("Inside loop: ", value);
+                                                        Toast.makeText(getApplicationContext(), "SMS Sent!",
+                                                                Toast.LENGTH_SHORT).show();
+                                                        Log.i("Log 4", "After toast");
+                                                        System.exit(0);
+                                                    }
+
+                                                    @Override
+                                                    public void onCancelled(DatabaseError databaseError) {
+
+                                                    }
+                                                });
+
+                                            } catch (Exception e) {
+                                                Toast.makeText(getApplicationContext(),
+                                                        "SMS faild, please try again later!",
+                                                        Toast.LENGTH_SHORT).show();
+                                                Log.i("Log 5", "Error");
+                                                e.printStackTrace();
+                                            }
+
+                                        }
+
                                         public void onCancelled(DatabaseError databaseError) {
+
 
                                         }
                                     });
 
-                                } catch (Exception e) {
-                                    Toast.makeText(getApplicationContext(),
-                                            "SMS faild, please try again later!",
-                                            Toast.LENGTH_LONG).show();
-                                    Log.i("Log 5", "Error");
-                                    e.printStackTrace();
                                 }
-
                             }
+
+                            @Override
                             public void onCancelled(DatabaseError databaseError) {
 
 
                             }
                         });
-
                     }
                 }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
+                    public void onCancelled(DatabaseError databaseError) {
 
 
-                }
-            });
+                    }
+                });
+
+
         }
+
 
 
     }
